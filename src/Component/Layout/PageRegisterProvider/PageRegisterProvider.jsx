e5198021f90436940b1501f2ad539bd09cf7593d
Carlos <ruizcarlosandre1@gmail.com>
2022-08-08 17:54:32 -0500

    cree el estili de la pagina de proveedor

--- src/Component/Layout/PageRegisterProvider/PageRegisterProvider.jsx
+++ src/Component/Layout/PageRegisterProvider/PageRegisterProvider.jsx
@@ -4,6 +4,8 @@ import { Service_Update } from '../../../service/ServiceProduct/Service_Update'
 import { Logo } from '../../Ui/Logo/Logo'
 import { DataTableProvider } from '../DataTableTemplatingDemo/DataTableProvider'
 import { Service_RegisterProvider } from '../../../service/ServiceProvider/Service_RegisterProvider'
+import './PageRegisterProvider.css'
+
 export const PageRegisterProvider = () => {
   
   const [value, setValue] = useState(false);
