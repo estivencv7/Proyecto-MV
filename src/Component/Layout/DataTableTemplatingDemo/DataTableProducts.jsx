d51e870784a3afa3284027d3187f3d411e94b318
unknown <SENA@ARMAPRRQDFSD971.FormacionSena.RED>
2022-08-08 12:16:19 -0500

    Cambios

--- src/Component/Layout/DataTableTemplatingDemo/DataTableProducts.jsx
+++ src/Component/Layout/DataTableTemplatingDemo/DataTableProducts.jsx
@@ -46,7 +46,7 @@ export const DataTableProducts = () => {
         const value = e.target.value;
         let _filters = { ...filters };
         _filters['global'].value = value;
-
+        console.log(selectedProducts);
         setFilters(_filters);
         setGlobalFilterValue(value);
     }
@@ -135,7 +135,7 @@ export const DataTableProducts = () => {
             <div className="card">
                 <DataTable value={products} paginator className="p-datatable-customers" header={header} rows={5}
                     paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown" rowsPerPageOptions={[10, 25, 50]}
-                    dataKey="id" rowHover selection={selectedProducts} onSelectionChange={e => setSelectedProduct(e.codigo_producto)}
+                    dataKey="id" rowHover selection={selectedProducts} onSelectionChange={e => setSelectedProduct(e.value)}
                     filters={filters} filterDisplay="menu" loading={loading} responsiveLayout="scroll"
                     globalFilterFields={['nombre_producto', 'codigo_producto', 'cantidad_producto', 'descripcion_producto', 'precio_producto' , 'id_categoria.nombre_categoria' , 'nombre_proveedor_producto']} emptyMessage="No se encontraron productos."
                     currentPageReportTemplate="Showing {first} to {last} of {totalRecords} entries">
