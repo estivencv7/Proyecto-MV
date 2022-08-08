945b216ad511a31b5b0d7aaae5b67ee85bc35243
unknown <SENA@ARMAPRRQDFSD971.FormacionSena.RED>
2022-08-08 16:23:30 -0500

    Cambios en el servicio de actualizacion

--- src/Component/Layout/DataTableTemplatingDemo/DataTableProducts.jsx
+++ src/Component/Layout/DataTableTemplatingDemo/DataTableProducts.jsx
@@ -147,9 +147,9 @@ export const DataTableProducts = () => {
     return (
         <div className="datatable-doc-demo">
             <div className="card">
-                <DataTable value={products} paginator className="p-datatable-customers" header={header} rows={5}
+                <DataTable value={products} paginator className="p-datatable-customers" header={header} rows={5} footer={footer}
                     paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown" rowsPerPageOptions={[10, 25, 50]}
-                    dataKey="id" rowHover selection={selectedProducts} onSelectionChange={e => setSelectedProduct(e.value)}
+                    dataKey="id" rowHover onSelectionChange={e => setSelectedProduct(e.value)}
                     filters={filters} filterDisplay="menu" loading={loading} responsiveLayout="scroll"
                     globalFilterFields={['nombre_producto', 'codigo_producto', 'cantidad_producto', 'descripcion_producto', 'precio_producto' , 'id_categoria.nombre_categoria' , 'nombre_proveedor_producto']} emptyMessage="No se encontraron productos."
                     currentPageReportTemplate="Showing {first} to {last} of {totalRecords} entries">
