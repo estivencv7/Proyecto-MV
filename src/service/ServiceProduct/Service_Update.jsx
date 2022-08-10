112ad0411bacd17e4019e6800d1e2458c61a2253
unknown <SENA@ARMAPRRQDFSD971.FormacionSena.RED>
2022-08-10 12:35:00 -0500

    Cambios en el formulario de actualizar

--- src/service/ServiceProduct/Service_Update.jsx
+++ src/service/ServiceProduct/Service_Update.jsx
@@ -56,6 +56,7 @@ export const Service_Update = ({codeProductUpdate}) => {
         setImageProduct(prod.foto_producto)
         inputPrice.setAttribute('id','inputPrice')
         inputPrice.setAttribute('type','number')
+        inputPrice.placeholder = prod.precio_producto
         inputDescription.placeholder = prod.descripcion_producto
         inputDescription.setAttribute('id','inputDescription')
         dataProductsContainer.append('Codigo')
