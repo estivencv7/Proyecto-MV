a2eb466d2d069ce90bb7ee05131f8a2ac2de16d4
estiven <stivencv7@gmailcom>
2022-08-11 11:30:13 -0500

    Agregando CarouselPrime

--- src/Component/Layout/ContentPageMain/ContentPageMain.jsx
+++ src/Component/Layout/ContentPageMain/ContentPageMain.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import CarouselDemo from './Carousel'
 import './CarouselDemo.css'
 import { Service_ListProduct } from '../../../service/ServiceProduct/Service_ListProduct'
+import { CarouselPrime } from './CarouselPrime'
+
 
 export const ContentPageMain = () => {
 
@@ -10,13 +12,16 @@ export const ContentPageMain = () => {
       <div className='content-page-main'>
          <CarouselDemo />
          {/*==============card=====================*/}
-         <div className='car'>
-
+          {/* <div className='car'>
+            <br /><br /><br />
             <Service_ListProduct />
 
-
-         </div>
-
+         </div>   */}
+      <div>
+        
+      </div>
+      
+       <CarouselPrime/>
 
       </div>
 
