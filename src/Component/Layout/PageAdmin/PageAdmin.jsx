0573ab063ee0c4c88507bf9155c18919ac548c47
Carlos <ruizcarlosandre1@gmail.com>
2022-08-12 17:37:43 -0500

    Arreglando pagina de administrador

--- src/Component/Layout/PageAdmin/PageAdmin.jsx
+++ src/Component/Layout/PageAdmin/PageAdmin.jsx
@@ -1,26 +1,20 @@
 import React from 'react'
 import { Logo } from '../../Ui/Logo/Logo'
 import { ContentPageMainAdmin } from '../InterfazAmind/ContentPageMainAdmin'
+import { NavigationAdmin } from '../NavigationAdmin/NavigationAdmin'
 import './style.css'
 
 export const PageAdmin = () => {
   return (
-    <div className='content'>
-      <div className='sildebarAdmin'>
-        <header className='he'>
-          <div className='divLogo'>
-            <Logo classN='logo' classTitle='title'/>
-          </div>
-        </header>
-      </div>
+    <div>
+      <NavigationAdmin/>
 
-      <div className='section'>
-        <header className='header-content' style={{height:'10em'}}>
-          <p>Muebles Valencia dfdfgd</p>
-        </header>
+      <div className='content'>
+      <div className='section'> 
          <div className='section-card'  style={{height:'100%'}}>
          <ContentPageMainAdmin/>
          </div>
+      </div>
       </div>
     </div>
   )
