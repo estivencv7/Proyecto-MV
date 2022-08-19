8e7dc0f844e1ce158ff6003346b0d95d758b74d8
JhonJairoChala <jhonjairochala8@gmail.com>
2022-08-18 21:12:58 -0500

    Subi el login para el inicio de sesion del usuario y el retorno del token

--- src/service/ServiceProduct/Service_ProductRegis.js
+++ src/service/ServiceProduct/Service_ProductRegis.js
@@ -4,7 +4,6 @@ import { Dialog } from 'primereact/dialog';
 import { Button,} from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
 import './css/registerProduct.css'
-import { axios } from 'axios'
 export const Service_ProductRegis = ({style}) => {
 
     const [visible, setVisible] = useState(false)
