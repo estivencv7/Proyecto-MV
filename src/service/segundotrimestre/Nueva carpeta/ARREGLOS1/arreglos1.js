caa57acb31dbb7359727d9906455053ebfffe18e
estiven <stivencv7@gmail.com>
2022-08-23 12:27:54 -0500

    guardando cambios

---
+++ src/service/segundotrimestre/Nueva carpeta/ARREGLOS1/arreglos1.js
@@ -0,0 +1,50 @@
+/*
+// ESTO NO SE HACE
+let lunes = "lunes";
+let martes = "martes";
+let miercoles = "miercoles";
+console.log("Dia actual :", lunes)
+*/
+// cada elemento del arreglo esta separado por comas
+// cada elemento puede ser del tipo que se prefiera o admita el lenguaje
+// aca el indice del elemento lunes es 0, el indice del elemento es jueves es 3
+//variable tipo arreglo, porque no tiene un string si no un arreglo
+/*
+let diasSemana= ["lunes","martes","miercoles","jueves","viernes","sabado","domingo"]
+//OPERACION DE LECTURA
+//para leer los elementos de un arreglo, usamos los indices pasandolos en corchetes antecedidos
+//del nombre del arreglo:
+console.log("Día 1 de la semana :",diasSemana[0]);
+let dia = diasSemana[5];
+console.log("Día 5 de la semana :",dia);
+
+//lectura de un elemento que no existe mediante un 
+//indice erroneo (fuera de rango)
+
+console.log("Dia 10 de la semana", diasSemana[9]);
+//ESCRITURA
+// usamos corchetes con el indice del elemento a sobreescribir, seguidos del operador asignacion (=)
+let mix=[true, "Hola", 34,20.7];
+console.log ("Arreglo mix antes de cambiar", mix);
+//vamos a cambiar el elemento true por el false
+mix[0] = false;
+console.log ("Arreglo mix antes de cambiar", mix);
+
+//OPERACION DE RECORRIDO
+//Se trata de recorrer cada elemento del arreglo usando
+//un ciclo. En este caso usaremos el ciclo Foreach
+let digitos= [0,1,2,3,4,5,6,7,8,9];
+//Para usar el foreach, usamos nombre del arreglo a recorrer, seguido del operador. Seguido de la palabra 
+//reservada foreach, seguido de la funcion a ejecutar el parametro 
+
+//(nota mia:es una funcion y elemnt es el parametro)
+
+digitos.forEach(elemento => {
+    console.log("Elemento :", elemento);
+ });
+ let nombres = ["Pepe", "Juan", "Maria"];
+ nombres.forEach(element => {
+     console.log ("Nombre: ", element);
+ });
+*/
+//
---
+++ src/service/segundotrimestre/Nueva carpeta/TALLERARREGLOS/tallerarreglos.js
@@ -0,0 +1,14 @@
+/*
+//Dado el siguiente arreglo let j = [200, -100, 45, 78, 32] , imprimir los elementos de indice 2 y 4
+let j = [200, -100, 45, 78, 32];
+console.log("Elementos indice 2:", j[2], "Elemento indice 4:",j[4])
+*/
+/*
+//Dado el siguiente arreglo let f = ["ab", "cd", "ef", "gh"] , imprimir los elementos cd y gh.
+let f = ["ab", "cd", "ef", "gh"]
+console.log("Elementos 1:", f[1], "Elemento 3:",f[3])
+*/
+//Dado el siguiente arreglo let aux = [10, true, "k200", 20.7] , 
+//imprimir todos sus elementos usando la operacion de recorrido con foreach
+let aux = [10, true, "k200", 20.7]
+
