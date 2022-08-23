a5948c6c0717cb98f4c57017ce5aaf2e9edd6000
estiven <stivencv7@gmail.com>
2022-08-23 16:55:57 -0500

    Implementando el registro de favoritos

--- src/Component/Layout/Favorites/Favorites.js
+++ src/Component/Layout/Favorites/Favorites.js
@@ -1,44 +1,44 @@
-import React from 'react'
+// import React from 'react'
 
-export const Favorites = ({codigo,nam,descriptionP,pressP,pressT,cantidadP,imagenP}) => {
-
-    
-    function lis(){
-        let gurdar=[]
-        return gurdar
-    }
+// export const Favorites = ({codigo,nam,descriptionP,pressP,pressT,cantidadP,imagenP}) => {
 
     
-    let gurdar2=[]
-    let gurdar3=[]
-    const saveLocalStore=()=>{
+//     function lis(){
+//         let gurdar=[]
+//         return gurdar
+//     }
+
+    
+//     let gurdar2=[]
+//     let gurdar3=[]
+//     const saveLocalStore=()=>{
        
 
-        let producto={
-            id:codigo,
-            nombreProduct:nam,
-            descriptionProduct:descriptionP,
-            pressProduct:pressP,
-            pressTotal:pressT,
-            cantidad:cantidadP,
-            image:imagenP
-        }
+//         let producto={
+//             id:codigo,
+//             nombreProduct:nam,
+//             descriptionProduct:descriptionP,
+//             pressProduct:pressP,
+//             pressTotal:pressT,
+//             cantidad:cantidadP,
+//             image:imagenP
+//         }
         
-        gurdar3.push(producto)
+//         gurdar3.push(producto)
 
-        localStorage.setItem("producto",  JSON.stringify(gurdar3) ); 
+//         localStorage.setItem("producto",  JSON.stringify(gurdar3) ); 
 
-        if(localStorage.getItem("producto")){
-            let producto=JSON.parse(localStorage.getItem("producto"));
-            console.log(producto)   
-        }else{
-            console.log("vacio")
+//         if(localStorage.getItem("producto")){
+//             let producto=JSON.parse(localStorage.getItem("producto"));
+//             console.log(producto)   
+//         }else{
+//             console.log("vacio")
 
-        }
-    }
+//         }
+//     }
 
    
-  return (
-    <button onClick={saveLocalStore}   className='heart'><i className='pi pi-heart heart-icon'></i></button>
-  )
-}
+//   return (
+//     <button onClick={saveLocalStore}   className='heart'><i className='pi pi-heart heart-icon'></i></button>
+//   )
+// }
