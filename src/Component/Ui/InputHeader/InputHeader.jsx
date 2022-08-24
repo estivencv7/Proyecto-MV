d5f01dd0c1a4610544559d7d1601972bee19aa93
Carlos <ruizcarlosandre1@gmail.com>
2022-08-24 14:07:17 -0500

    el input busca por categoria

--- src/Component/Ui/InputHeader/InputHeader.jsx
+++ src/Component/Ui/InputHeader/InputHeader.jsx
@@ -4,7 +4,7 @@ import {AutoComplete}from 'rsuite'
 import { InputText } from 'primereact/inputtext';
 import './style.css'
 import { Input } from '@material-ui/core';
-import { GetCards } from '../../Layout/ContentPageMain/GetCards';
+import { GetCardsCategory } from '../../Layout/ContentPageMain/GetCardsCategory';
 export const InputHeader = ({id}) => {
   
   // const [category, setCategory] = useState('');
@@ -12,10 +12,11 @@ export const InputHeader = ({id}) => {
   const [filter, setFilters] = useState('');
   const [products, setProducts] = useState([]);
 
-function filtersCategory(category) {
-
+function filtersCategory() {
+  console.log("CATEGORIA " );
+  console.log(document.getElementById("inpuText").value);
   console.log("LISTANDO PRODUCTOS")
-  const urlRegister = 'http://localhost:8080/producto/filtrarCategoria/' + category;
+  const urlRegister = 'http://localhost:8080/producto/filtrarCategoria/' + document.getElementById("inpuText").value;
   fetch(urlRegister, {
       method: 'POST',
       headers: {
@@ -23,11 +24,28 @@ function filtersCategory(category) {
       }
   })
       .then(response => response.json())
-      .then(data => setProducts(data))
+      .then(data => {
+        setProducts(data)
+        createElements()
+      })
 
 
 }
 
+function createElements(){
+  <GetCardsCategory getCards={products}/>
+  const categoryContainer = document.getElementById("categoryContainer")
+  if(document.getElementById("conter_car") == null){
+  const div = document.createElement("div")
+  if(document.getElementById("categoryContainer") == null){
+      document.getElementById("conter_car").replaceWith(div)
+  }else if(document.getElementById("conter_car") == null){
+      document.getElementById("categoryContainer").replaceWith(div)
+  }
+  }else{
+    document.getElementById("conter_car").replaceWith(categoryContainer)
+  }
+}
 // function listProducts() {
 
 //   console.log("LISTANDO PRODUCTOS")
@@ -42,16 +60,6 @@ function filtersCategory(category) {
 //       .then(product => setProducts(product))
 // }
 // products.forEach(element => {
-  console.log("LISTANDO PRODUCTOS")
-  const urlRegister = 'http://localhost:8080/producto/listaProductos';
-  fetch(urlRegister, {
-      method: 'GET',
-      headers: {
-          'Content-Type': 'application/json'
-      }
-  })
-      .then(response => response.json())
-      .then(product => setProducts(product))
   // setLoading(false)
 // // array.forEach(element => {
 // array.forEach(element => {
@@ -67,9 +75,9 @@ function filtersCategory(category) {
   
   return (
     <div className='input-wrapper'>
-      <i className="pi pi-search input-icon"></i>
-     <InputText id={id} onChange={e => filtersCategory(e.target.value)} placeholder='¿Qué estás buscando?' className='autoInput' ></InputText>
-     <GetCards getCards={products}/>
+      <i className="pi pi-search input-icon" onClick={filtersCategory}></i>
+     <InputText  id='inpuText' placeholder='¿Qué estás buscando?' className='autoInput' ></InputText>
+     <GetCardsCategory getCards={products}/>
     </div>
   )
 }
