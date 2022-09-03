aee57e23efb6200d80bf9d4bf4ea37d9287307b5
JhonJairoChala <jhonjairochala8@gmail.com>
2022-09-02 21:31:11 -0500

    Subi el link de descarga para las facturas (Falta por terminar algunos detalles en cuanto a comprobar y la pagina en que aparece)

---
+++ src/service/ServiceInvoice/Service_ListInvoices.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Service_ListInvoices = () => {
+    
+    
+  return (
+    <div>
+        <button><a href="http://localhost:8080/facturas/listaFacturas">Facturas</a></button>
+    </div>
+  )
+}
