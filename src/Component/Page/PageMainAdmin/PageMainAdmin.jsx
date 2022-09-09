8d674196f3e8e419c1b17c5276d9260adf0eb48b
unknown <SENA@ARMAPRRQDFSD971.FormacionSena.RED>
2022-09-09 13:50:39 -0500

    Agregue alertas que faltaban para confirmar algunas acciones

--- src/Component/Page/PageMainAdmin/PageMainAdmin.jsx
+++ src/Component/Page/PageMainAdmin/PageMainAdmin.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import toast from 'react-hot-toast'
 import { PageAdmin } from '../../Layout/PageAdmin/PageAdmin'
 import './style.css'
+
 export const PageMainAdmin = () => {
+  useEffect(() => {
+    const admin = sessionStorage.getItem("administrador")
+    const admin2 = JSON.parse(admin.toString());
+    document.getElementById("nameAccount").textContent = admin2.nameU
+    toast("Has iniciado sesion, Bienvenido " + admin2.nameU)
+  }, [])
   return (
     <div className='bodyPagemainAdmin'>
         <PageAdmin/>
